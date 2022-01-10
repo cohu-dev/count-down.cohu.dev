4a15ddc4991a9563f059876d96d02d10e6afc923
cota <65804288+cota-eng@users.noreply.github.com>
2022-01-10 18:28:35 +0900

    add twitter demo link

--- components/Flow/Embed.tsx
+++ components/Flow/Embed.tsx
@@ -21,7 +21,15 @@ const Embed = () => {
           <p className="leading-relaxed">
             「/embed」と打ち込むと出てきます。
             <br />
-            そのURL貼り付け欄にコピーしたURLを貼り付けます。
+            そのURL貼り付け欄にコピーしたURLを貼り付けます。 やり方は
+            <a
+              target="_blank"
+              className="underline text-blue-400"
+              href="https://twitter.com/cohu_dev/status/1480430003485831171"
+            >
+              こちら
+            </a>
+            から確認できます。
           </p>
         </div>
       </div>
