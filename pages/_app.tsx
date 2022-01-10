3f145e7099ad9c9c15765fd7e5761d156f0f7b30
cota <65804288+cota-eng@users.noreply.github.com>
2022-01-10 16:56:21 +0900

    fix typo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,17 +3,16 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 const Description =
   "Notionでカウントダウンを行うためのサイトです。手順はたったの3ステップです。日時を入力し、生成されたURLをNotionに貼り付けるだけ。";
+const Title = "Notionカウントダウンメーカー";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
       <Head>
-        <title>
-          Notonカウントダウンメーカー - 大事な予定をカウントダウンしよう
-        </title>
+        <title>{Title} - 大事な予定をカウントダウンしよう</title>
         <meta name="description" content={Description}></meta>
         <link rel="icon" href="/favicon.ico" />
-        <meta property="og:title" content="Notonカウントダウンメーカー" />
+        <meta property="og:title" content={Title} />
         <meta property="og:description" content={Description} />
         <meta property="og:url" content={process.env.NEXT_PUBLIC_URL} />
         <meta
@@ -21,7 +20,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           content={`${process.env.NEXT_PUBLIC_URL}/ogp.png`}
         />
         ;
-        <meta property="og:site_name" content="Notonカウントダウンメーカー" />
+        <meta property="og:site_name" content={Title} />
         <meta property="og:locale" content="ja_JP" />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
