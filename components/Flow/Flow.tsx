412505a603f281690482930182ad15f7b39832e7
cota <65804288+cota-eng@users.noreply.github.com>
2022-01-11 18:00:40 +0900

    responsive fixed

--- components/Flow/Flow.tsx
+++ components/Flow/Flow.tsx
@@ -5,11 +5,11 @@ import Embed from "./Embed";
 import DateTimePick from "./DateTimePick";
 import Countdown from "../Countdown";
 import Share from "./Share";
-const afterOneHour = new Date();
-afterOneHour.setHours(afterOneHour.getHours() + 1);
+const afterTwoHour = new Date();
+afterTwoHour.setHours(afterTwoHour.getHours() + 2);
 
 const Flow: React.VFC = () => {
-  const [due, setDue] = useState<Date>(afterOneHour);
+  const [due, setDue] = useState<Date>(afterTwoHour);
   const [url, setUrl] = useState<string>("");
   useEffect(() => {
     setUrl(`${process.env.NEXT_PUBLIC_URL}/watch?t=${due.getTime()}`);
@@ -17,7 +17,7 @@ const Flow: React.VFC = () => {
   return (
     <section className="text-gray-600 body-font">
       <Countdown t={due} />
-      <div className="md:px-5 py-2 mx-auto flex flex-wrap">
+      <div className="md:px-5 py-2 w-full lg:w-1/2 mx-auto flex flex-wrap">
         <DateTimePick due={due} setDue={setDue} />
         <CopyURL url={url} />
         <OpenNotion />
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
   theme: {
     screens: {
       xs: "480px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
     },
   },
   plugins: [],
