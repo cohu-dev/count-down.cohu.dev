e4fddf397338cfcf10df922f73d61ba4f0a5029e
cohu-dev <hiraikota4649@gmail.com>
2023-03-04 10:35:24 +0900

    test: vercel server side render

---
+++ pages/watch/[t].tsx
@@ -0,0 +1,47 @@
+import { GetServerSideProps, NextPage } from "next";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import useInterval from "use-interval";
+import Card from "../../components/Card";
+
+interface Props {
+  t: string;
+}
+const watch: NextPage<Props> = ({ t }) => {
+  const router = useRouter();
+  if (!t) return <></>;
+  const replacedT = t.toString().replace(/\//g, "");
+  const now = Date.now();
+  const [time, setTime] = useState(0);
+  useInterval(() => {
+    setTime(() => Number(replacedT) - now);
+    if (isNaN(+replacedT)) router.push("/");
+  }, 1000);
+  if (time < 0)
+    return (
+      <div className="h-screen flex items-center justify-center text-2xl">
+        このカウントダウンは既に終了しています。
+        <br />
+        仕様変更によるエラーの場合ご迷惑おかけしますが、再度設定をお願い致します。
+      </div>
+    );
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div className="grid grid-cols-4 gap-2">
+        <Card n={Math.floor(time / (1000 * 60 * 60 * 24))} digit="Days" />
+        <Card n={new Date(time).getUTCHours()} digit="Hours" />
+        <Card n={new Date(time).getMinutes()} digit="Minutes" />
+        <Card n={new Date(time).getSeconds()} digit="Seconds" />
+      </div>
+    </div>
+  );
+};
+
+export default watch;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const t = context.query.t;
+  return {
+    props: { t },
+  };
+};
