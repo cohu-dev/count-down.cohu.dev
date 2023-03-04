7d8f1edd067b8ff32d784a3b85a479a4e7498b3e
cohu-dev <hiraikota4649@gmail.com>
2023-03-04 11:13:58 +0900

    test: csr

--- pages/watch.tsx
+++ pages/watch.tsx
@@ -26,6 +26,12 @@ const watch: NextPage = () => {
   return (
     <div className="h-screen flex items-center justify-center">
       <div className="grid grid-cols-4 gap-2">
+        <Head>
+          <meta
+            property="og:url"
+            content={`${process.env.NEXT_PUBLIC_URL}/watch/${t}`}
+          ></meta>
+        </Head>
         <Card n={Math.floor(time / (1000 * 60 * 60 * 24))} digit="Days" />
         <Card n={new Date(time).getUTCHours()} digit="Hours" />
         <Card n={new Date(time).getMinutes()} digit="Minutes" />
